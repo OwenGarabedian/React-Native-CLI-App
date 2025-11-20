6385e5f1372e8f5a8987f31009ed8a77412203ad
OwenGarabedian <owengarabedian@gmail.com>
2025-11-20 16:31:12 -0500

    Added landing page for users that log in successfully.

--- screens/LandingPage.tsx
+++ screens/LandingPage.tsx
@@ -2,17 +2,66 @@ import { View, Text, Dimensions, StyleSheet, Button, TouchableOpacity, Pressable
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const textValue = "Enter Log In Code"
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 
-const LogIn = () => {
-    return (
+// const childContainerWidth = screenWidth;
+const horizontalBoarderHeightTop = screenHeight * .2;
+
+
+const topChildContainerHeight = screenHeight * .35;
+const overviewWidth = screenWidth * .85;
+const overviewHeight = topChildContainerHeight * .8;
+
+const twoButtonChildContainerHeight = screenHeight * .25;
+const bottomButtonsWidth = screenWidth * .4;
+
+const horizontalBoarderHeightBottom = screenHeight * .15;
+
+
+
+// const 
+
+
+const LandingPage = () => {
+    return(
         <View style={Styles.container}>
-            <Text>Logged In!</Text>
+            <View style={Styles.horizontalBoarderTop}></View>
+
+            <View style={Styles.childConatinerTop}>
+                <View style={Styles.overviewContainer}></View>
+
+            </View>
+            {/* <View style={Styles.middleDividerContainer}>
+
+            </View> */}
+            <View style={Styles.childConatinerBottom}>
+
+
+
+                            <Pressable     
+                style={({ pressed }) => pressed ? Styles.dataTableButtonPressed : Styles.dataTableButtonNormal}
+            >
+               <Text style={Styles.dataTableButtonText}>DATA{"\n"}TABLE</Text>
+               
+            </Pressable>
+
+            <Pressable     
+                style={({ pressed }) => pressed ? Styles.dataTableButtonPressed : Styles.dataTableButtonNormal}
+            >
+               <Text style={Styles.dataTableButtonText}>LOG{"\n"}BOOK</Text>
+               
+            </Pressable>
+
+            </View>
+            <View style={Styles.horizontalBoarderBottom}>
+
+            </View>
         </View>
-    );
+    )
 }
 
-export default LogIn
+export default LandingPage
 
 const Styles = StyleSheet.create({
     container: {
@@ -21,4 +70,70 @@ const Styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+
+    horizontalBoarderTop: {
+        height: horizontalBoarderHeightTop,
+        width: screenWidth,
+        backgroundColor: '#ff0000ff',
+    },
+
+    childConatinerTop: {
+        width: screenWidth,
+        height: topChildContainerHeight,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ff8000ff',
+    },
+
+    childConatinerBottom: {
+        backgroundColor: '#04ff00ff',
+        width: screenWidth,
+        height: twoButtonChildContainerHeight,
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+
+    },
+    overviewContainer: {
+        height: overviewHeight,
+        width: overviewWidth,
+        borderRadius: 18,
+        backgroundColor: '#ffffffff',
+    },
+
+    horizontalBoarderBottom: {
+        height: horizontalBoarderHeightBottom,
+        width: screenWidth,
+        backgroundColor: '#002affff',
+    },
+
+
+
+    dataTableButtonNormal: {
+        width: bottomButtonsWidth,
+
+
+
+        backgroundColor: '#111111ff',
+        borderRadius: 8,
+        alignItems: 'center',
+        opacity: 1,
+        justifyContent: 'center'
+    },
+    dataTableButtonPressed: {
+        width: bottomButtonsWidth,
+
+
+
+        backgroundColor: '#111111ff',
+        borderRadius: 8,
+        alignItems: 'center',
+        opacity: .75,
+        justifyContent: 'center'
+    },
+    dataTableButtonText: {
+        color: '#fff',
+        fontSize: 36,
+        fontWeight: '600',
+        textAlign: 'center',
+    }
 })
